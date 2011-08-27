8777dad780638680ebb7ca8eb66a932d2deb3eb5
Troy Whiteley <dawnerd@gmail.com>
2011-08-26 20:12:51 -0700

    Added deploy script.

--- server.js
+++ server.js
@@ -19,11 +19,13 @@ app.configure(function(){
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  var server_port = 3000;
 });
 
 app.configure('production', function(){
   app.use(express.errorHandler()); 
+  var server_port = 80;
 });
 
 // Routes
@@ -34,5 +36,5 @@ app.get('/', function(req, res){
   });
 });
 
-app.listen(3000);
+app.listen(server_port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
