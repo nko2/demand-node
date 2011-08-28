3a10b29a08e5793dee7a1056a62ea838adf26c51
Michael Dyer <mdyer@n9nemedia.net>
2011-08-28 11:59:27 -0700

    moved nowplaying/currentplaying again

--- public/ui/shared/scripts/controllers/room.js
+++ public/ui/shared/scripts/controllers/room.js
@@ -71,6 +71,7 @@ var RoomController = Fidel.ViewController.extend({
       var tmp = $("#tmpSelectedTrack").html();
       var html = str.template(tmp, { track: data });
       self.find("#selectedTrack").html(html);
+      self.find("#selectedTrack").show('block');
     });
   },
   playTrackAction: function(e) {
@@ -88,6 +89,7 @@ var RoomController = Fidel.ViewController.extend({
       var tmp = $("#tmpNowPlaying").html();
       var html = str.template(tmp, { track: data });
       self.find("#nowPlaying").html(html);
+      self.find("#nowPlaying").show('block');
     });
   },
   placeBid: function(e) {
