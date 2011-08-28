d8efbaa378c47dd64706764719d6a4e0324644a1
Michael Dyer <mdyer@n9nemedia.net>
2011-08-28 10:44:50 -0700

    Updated search if no results. Moved selected track

--- public/ui/shared/scripts/controllers/search.js
+++ public/ui/shared/scripts/controllers/search.js
@@ -17,12 +17,17 @@ var SearchController = Fidel.ViewController.extend ({
   results: function(data){
     var results = data.results;
     this.resultsNode.html('');
-    for(i = 0, c = results.length; i < c; i++){
-      if (results[i].canStream) {
-        this.resultsNode.append('<li class="song" data-trackkey='+ results[i].key +'>' + results[i].artist + ' - ' + results[i].name + ' ('+ results[i].album+')</li>');
-      }
+    console.log(data);
+    if(results.length == 0) {
+        this.resultsNode.append('<li><span>Sorry, brah. Stairway to heaven is nigh. Please find another track.</span></li>')
+    } else {
+      for(i = 0, c = results.length; i < c; i++){
+        if (results[i].canStream) {
+          this.resultsNode.append('<li class="song" data-trackkey='+ results[i].key +'>' + results[i].artist + ' - ' + results[i].name + ' ('+ results[i].album+')</li>');
+        }
+      } 
+      this.resultsNode.append('<li class="hide">Hide Results</li>');
     }
-    this.resultsNode.append('<li class="hide">Hide Results</li>');
   },
   select: function(e) {
     var trackKey = e.target.getAttribute('data-trackkey');
